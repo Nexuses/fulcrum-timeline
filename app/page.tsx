0a15a38d0cab8914a0f06a4f7de20956a28bedfc
arpitnexuses <arpit.m@nexuses.in>
2025-10-31 14:59:13 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Timeline from "@/components/timeline"
 
 export default function Page() {
   return (
-    <div className="bg-white m-0 p-0">
+    <div className="bg-white m-0 p-0 mt-[35px]">
       <Timeline />
     </div>
   )
